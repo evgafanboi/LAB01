42a029e9b64cf88fa861bfd44d9e5ef2ccc2c8b6
evgafanboi <22521115@gm.uit.edu.vn>
2024-03-28 06:39:21 +0700

    Added main form

--- BTTH2/LAB2.cs
+++ BTTH2/LAB2.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BTTH2
 {
-    public partial class Form1 : Form
+    public partial class LAB2 : Form
     {
-        public Form1()
+        public LAB2()
         {
             InitializeComponent();
         }

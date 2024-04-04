ea12dc52ac39d1ce5d39076485028810e5a6b04f
Vinh Huynh <22521668@gm.uit.edu.vn>
2024-04-04 22:34:19 +0700

    Push additional changes

--- BTTH2/BAI04.Designer.cs
+++ BTTH2/BAI04.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.TextBoxScore1 = new System.Windows.Forms.TextBox();
             this.TextBoxPhoneNum = new System.Windows.Forms.TextBox();
-            this.TextBoxMSSV = new System.Windows.Forms.TextBox();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
             this.TextBoxScore2 = new System.Windows.Forms.TextBox();
             this.TextBoxScore3 = new System.Windows.Forms.TextBox();
             this.TextBoxAvg = new System.Windows.Forms.TextBox();
@@ -51,14 +51,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TextBoxNameRead = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.ButtonWrite = new System.Windows.Forms.Button();
             this.ButtonRead = new System.Windows.Forms.Button();
             this.ButtonAdd = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.ComboBoxStudent = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ButtonCalculateAvg = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TextBoxScore1
@@ -67,6 +68,7 @@
             this.TextBoxScore1.Name = "TextBoxScore1";
             this.TextBoxScore1.Size = new System.Drawing.Size(100, 20);
             this.TextBoxScore1.TabIndex = 1;
+            this.TextBoxScore1.TextChanged += new System.EventHandler(this.TextBoxScore1_TextChanged);
             // 
             // TextBoxPhoneNum
             // 
@@ -74,13 +76,15 @@
             this.TextBoxPhoneNum.Name = "TextBoxPhoneNum";
             this.TextBoxPhoneNum.Size = new System.Drawing.Size(100, 20);
             this.TextBoxPhoneNum.TabIndex = 2;
+            this.TextBoxPhoneNum.TextChanged += new System.EventHandler(this.TextBoxPhoneNum_TextChanged);
             // 
-            // TextBoxMSSV
+            // TextBoxName
             // 
-            this.TextBoxMSSV.Location = new System.Drawing.Point(118, 131);
-            this.TextBoxMSSV.Name = "TextBoxMSSV";
-            this.TextBoxMSSV.Size = new System.Drawing.Size(100, 20);
-            this.TextBoxMSSV.TabIndex = 3;
+            this.TextBoxName.Location = new System.Drawing.Point(118, 131);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxName.TabIndex = 3;
+            this.TextBoxName.TextChanged += new System.EventHandler(this.TextBoxMSSV_TextChanged);
             // 
             // TextBoxScore2
             // 
@@ -88,6 +92,7 @@
             this.TextBoxScore2.Name = "TextBoxScore2";
             this.TextBoxScore2.Size = new System.Drawing.Size(100, 20);
             this.TextBoxScore2.TabIndex = 4;
+            this.TextBoxScore2.TextChanged += new System.EventHandler(this.TextBoxScore2_TextChanged);
             // 
             // TextBoxScore3
             // 
@@ -95,6 +100,7 @@
             this.TextBoxScore3.Name = "TextBoxScore3";
             this.TextBoxScore3.Size = new System.Drawing.Size(100, 20);
             this.TextBoxScore3.TabIndex = 5;
+            this.TextBoxScore3.TextChanged += new System.EventHandler(this.TextBoxScore3_TextChanged);
             // 
             // TextBoxAvg
             // 
@@ -117,9 +123,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(318, 105);
+            this.richTextBox1.Location = new System.Drawing.Point(325, 105);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(274, 387);
+            this.richTextBox1.Size = new System.Drawing.Size(267, 321);
             this.richTextBox1.TabIndex = 15;
             this.richTextBox1.Text = "";
             // 
@@ -269,13 +275,13 @@
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 25;
             // 
-            // textBox4
+            // TextBoxNameRead
             // 
-            this.textBox4.Location = new System.Drawing.Point(771, 127);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 24;
+            this.TextBoxNameRead.Location = new System.Drawing.Point(771, 127);
+            this.TextBoxNameRead.Name = "TextBoxNameRead";
+            this.TextBoxNameRead.ReadOnly = true;
+            this.TextBoxNameRead.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxNameRead.TabIndex = 24;
             // 
             // textBox5
             // 
@@ -295,39 +301,35 @@
             // 
             // ButtonWrite
             // 
+            this.ButtonWrite.Enabled = false;
             this.ButtonWrite.Location = new System.Drawing.Point(98, 45);
             this.ButtonWrite.Name = "ButtonWrite";
             this.ButtonWrite.Size = new System.Drawing.Size(120, 51);
             this.ButtonWrite.TabIndex = 34;
-            this.ButtonWrite.Text = "Write";
+            this.ButtonWrite.Text = "Write FIle";
             this.ButtonWrite.UseVisualStyleBackColor = true;
+            this.ButtonWrite.Click += new System.EventHandler(this.ButtonWrite_Click);
             // 
             // ButtonRead
             // 
+            this.ButtonRead.Enabled = false;
             this.ButtonRead.Location = new System.Drawing.Point(751, 45);
             this.ButtonRead.Name = "ButtonRead";
             this.ButtonRead.Size = new System.Drawing.Size(120, 51);
             this.ButtonRead.TabIndex = 35;
-            this.ButtonRead.Text = "Read";
+            this.ButtonRead.Text = "Read File";
             this.ButtonRead.UseVisualStyleBackColor = true;
             // 
             // ButtonAdd
             // 
-            this.ButtonAdd.Location = new System.Drawing.Point(98, 475);
+            this.ButtonAdd.Enabled = false;
+            this.ButtonAdd.Location = new System.Drawing.Point(38, 475);
             this.ButtonAdd.Name = "ButtonAdd";
             this.ButtonAdd.Size = new System.Drawing.Size(120, 51);
             this.ButtonAdd.TabIndex = 36;
             this.ButtonAdd.Text = "Add";
             this.ButtonAdd.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(751, 475);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 51);
-            this.button1.TabIndex = 37;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
             // 
             // ComboBoxStudent
             // 
@@ -335,19 +337,41 @@
             this.ComboBoxStudent.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.ComboBoxStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ComboBoxStudent.FormattingEnabled = true;
-            this.ComboBoxStudent.Location = new System.Drawing.Point(359, 505);
+            this.ComboBoxStudent.Location = new System.Drawing.Point(681, 488);
             this.ComboBoxStudent.Name = "ComboBoxStudent";
             this.ComboBoxStudent.Size = new System.Drawing.Size(190, 24);
             this.ComboBoxStudent.TabIndex = 38;
             this.ComboBoxStudent.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(691, 458);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(180, 20);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Select Student From file";
+            // 
+            // ButtonCalculateAvg
+            // 
+            this.ButtonCalculateAvg.Enabled = false;
+            this.ButtonCalculateAvg.Location = new System.Drawing.Point(198, 475);
+            this.ButtonCalculateAvg.Name = "ButtonCalculateAvg";
+            this.ButtonCalculateAvg.Size = new System.Drawing.Size(120, 51);
+            this.ButtonCalculateAvg.TabIndex = 40;
+            this.ButtonCalculateAvg.Text = "Tính TB";
+            this.ButtonCalculateAvg.UseVisualStyleBackColor = true;
+            this.ButtonCalculateAvg.Click += new System.EventHandler(this.ButtonCalculateAvg_Click);
             // 
             // BAI04
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(953, 561);
+            this.Controls.Add(this.ButtonCalculateAvg);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.ComboBoxStudent);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.ButtonAdd);
             this.Controls.Add(this.ButtonRead);
             this.Controls.Add(this.ButtonWrite);
@@ -360,7 +384,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.TextBoxNameRead);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.LabelAvg);
@@ -374,7 +398,7 @@
             this.Controls.Add(this.TextBoxAvg);
             this.Controls.Add(this.TextBoxScore3);
             this.Controls.Add(this.TextBoxScore2);
-            this.Controls.Add(this.TextBoxMSSV);
+            this.Controls.Add(this.TextBoxName);
             this.Controls.Add(this.TextBoxPhoneNum);
             this.Controls.Add(this.TextBoxScore1);
             this.Name = "BAI04";
@@ -388,7 +412,7 @@
         #endregion
         private System.Windows.Forms.TextBox TextBoxScore1;
         private System.Windows.Forms.TextBox TextBoxPhoneNum;
-        private System.Windows.Forms.TextBox TextBoxMSSV;
+        private System.Windows.Forms.TextBox TextBoxName;
         private System.Windows.Forms.TextBox TextBoxScore2;
         private System.Windows.Forms.TextBox TextBoxScore3;
         private System.Windows.Forms.TextBox TextBoxAvg;
@@ -409,13 +433,14 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TextBoxNameRead;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button ButtonWrite;
         private System.Windows.Forms.Button ButtonRead;
         private System.Windows.Forms.Button ButtonAdd;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox ComboBoxStudent;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button ButtonCalculateAvg;
     }
 }
--- BTTH2/BAI04.cs
+++ BTTH2/BAI04.cs
@@ -36,5 +36,113 @@ namespace BTTH2
         {
 
         }
+
+        private void TextBoxMSSV_TextChanged(object sender, EventArgs e)
+        {
+            // enable button to add student 
+            ButtonAdd.Enabled = true;
+
+        }
+
+        private void ButtonWrite_Click(object sender, EventArgs e)
+            
+        {
+            
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            // disable the button until user have input all the required fields except for the AvgTextBox
+            if (TextBoxName.Text == "" || TextBoxName.Text == "" || TextBoxPhoneNum.Text == "" || TextBoxScore1.Text == "" || TextBoxScore2.Text == "" || TextBoxScore3.Text == "")
+            {
+                MessageBox.Show("Chưa điền đủ thông tin", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // check if the input is valid for all scores score must be larger or equal to 0 and smaller or equal to 10
+            try
+            {
+                if (int.Parse(TextBoxScore1.Text) < 0 || int.Parse(TextBoxScore1.Text) > 10 || int.Parse(TextBoxScore2.Text) < 0 || int.Parse(TextBoxScore2.Text) > 10 || int.Parse(TextBoxScore3.Text) < 0 || int.Parse(TextBoxScore3.Text) > 10)
+                {
+                    MessageBox.Show("Điểm phải trong hệ 10", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            // catch exception if the input is not a number
+            catch (Exception)
+            {
+                MessageBox.Show("Điểm không hợp lệ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // name must not contain any character that is not a letter or a space
+            foreach (char c in TextBoxName.Text)
+            {
+                if (!char.IsLetter(c) && c != ' ')
+                {
+                    MessageBox.Show("Tên không hợp lệ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+     
+
+            
+
+
+        }
+
+        private void TextBoxPhoneNum_TextChanged(object sender, EventArgs e)
+        {
+            // enable button to add student 
+            ButtonAdd.Enabled = true;
+        }
+
+        private void TextBoxScore1_TextChanged(object sender, EventArgs e)
+        {
+            // enable button to add student 
+            ButtonAdd.Enabled = true;
+            ButtonCalculateAvg.Enabled = true;
+
+        }
+
+        private void TextBoxScore2_TextChanged(object sender, EventArgs e)
+        {
+            // enable button to add student 
+            ButtonAdd.Enabled = true;
+            ButtonCalculateAvg.Enabled = true;
+
+        }
+
+        private void TextBoxScore3_TextChanged(object sender, EventArgs e)
+        {
+            // enable button to add student 
+            ButtonAdd.Enabled = true;
+            ButtonCalculateAvg.Enabled = true;
+
+        }
+
+        private void ButtonCalculateAvg_Click(object sender, EventArgs e)
+        {
+            //calculate the average score from the 3 scores and check for invalid input scores must be larger or equal to 0 and smaller or equal to 10
+            try
+            {
+                if (int.Parse(TextBoxScore1.Text) < 0 || int.Parse(TextBoxScore1.Text) > 10 || int.Parse(TextBoxScore2.Text) < 0 || int.Parse(TextBoxScore2.Text) > 10 || int.Parse(TextBoxScore3.Text) < 0 || int.Parse(TextBoxScore3.Text) > 10)
+                {
+                    MessageBox.Show("Điểm phải trong hệ 10", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //calculate the average score and update the AvgTextBox
+                TextBoxAvg.Text = ((float)((int.Parse(TextBoxScore1.Text) + int.Parse(TextBoxScore2.Text) + int.Parse(TextBoxScore3.Text)) / 3)).ToString();
+            }
+            //catch exception if the input is not a number
+            catch (Exception)
+            {
+                MessageBox.Show("Điểm không hợp lệ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
     }
 }

0ed974b22853c6c6d304b199c70055b23e3995de
evgafanboi <163389269+evgafanboi@users.noreply.github.com>
2024-04-06 16:57:29 +0700

    ignoring DBInit() because low iq

--- BTTH2/Program.cs
+++ BTTH2/Program.cs
@@ -17,7 +17,7 @@ namespace BTTH2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LAB2());
-            DBInit();
+            //DBInit();
         }
         static void DBInit()
         {

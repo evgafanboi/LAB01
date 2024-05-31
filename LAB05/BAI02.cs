e85d7818d2a711b93876bb56038b4316e050026e
Vinh Huynh <22521668@gm.uit.edu.vn>
2024-05-31 20:09:51 +0700

    Test commit

---
+++ LAB05/BAI02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB05
+{
+    public partial class BAI02 : Form
+    {
+        public BAI02()
+        {
+            InitializeComponent();
+        }
+    }
+}

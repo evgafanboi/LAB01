119bc2cbf08eacea099522212ad91dc2d0e6c4d2
evgafanboi <22521115@gm.uit.edu.vn>
2024-06-03 22:32:11 +0700

    Database for BAI05 added. BAI05 is still halfway.

---
+++ LAB05/BAI05.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit;
+using MimeKit;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using MailKit.Net.Imap;
+using MailKit.Net.Pop3;
+using MailKit.Search;
+using System.Linq;
+using static System.Data.Entity.Migrations.Model.UpdateDatabaseOperation;
+
+namespace LAB05
+{
+    public partial class BAI05 : Form
+    {
+        private readonly BAI05DBContext _context;
+
+        private List<MimeMessage> emails;
+        public BAI05(BAI05DBContext context)
+        {
+            InitializeComponent();
+            // Ensure the database is created if not create one
+            _context = context;
+        }
+
+        private async void Loginbtn_Click(object sender, EventArgs e)   //cilb sxxp wstx bzpy   //sm0kew33d3v3ryd4y@gmail.com
+        {
+                if (listView1.Items.Count > 0)
+                    listView1.Items.Clear();
+                ImapClient client = new ImapClient();
+                try
+                {
+                    await client.ConnectAsync("imap.gmail.com", 993, true);
+                }
+                catch
+                {
+                    MessageBox.Show("Can't connect to server");
+                }
+                //auth
+                try
+                {
+                    await client.AuthenticateAsync(TextBoxEmail.Text, TextBoxPassword.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Can't authenticate");
+                }
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadWrite);
+
+                Loginbtn.Text = "Reload";
+
+                emails = new List<MimeMessage>();
+                foreach (var uid in inbox.Search(SearchQuery.NotSeen))
+                {
+                    var message = inbox.GetMessage(uid);
+                    if (message.Subject == "Đóng góp món ăn")
+                    {
+                        var email = new Email
+                        {
+                            Subject = message.Subject,
+                            Body = message.TextBody,
+                            IsRead = true,
+                            sender = message.Sender.ToString()
+                        };
+                        _context.Email.Add(email);
+                        await _context.SaveChangesAsync();
+
+                        var lines = message.TextBody.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int i = 0; i < lines.Length; i += 2)
+                        {
+                            var monAn = new MonAn
+                            {
+                                TenMonAn = lines[i],
+                                imageURI = lines[i + 1],
+                                Id_Email = email.Id_Email
+                            };
+                            _context.MonAn.Add(monAn);
+                        }
+
+                        _context.SaveChanges();
+                        inbox.AddFlags(uid, MessageFlags.Seen, true);
+                    }
+
+                client.Disconnect(true);
+
+                }
+        }
+
+        private void Exitbtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ LAB05/BAI05DBContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB05
+{
+    public class BAI05DBContext : DbContext
+    {
+        public DbSet<Email> Email {  get; set; }
+        public DbSet<MonAn> MonAn { get; set; }
+
+        public BAI05DBContext() : base("name=BAI05Context")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Email>()
+                .HasKey(em => em.Id_Email);
+
+            modelBuilder.Entity<MonAn>()
+                .HasKey(ma => ma.Id_MonAn);
+
+            modelBuilder.Entity<Email>()
+                .HasMany(em => em.MonAn)
+                .WithRequired(ma => ma.Email)
+                .HasForeignKey(ma => ma.Id_Email)
+                .WillCascadeOnDelete(false);    // the bound entity wont be erased if the the other side of the bind is deleted
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+    public class Email
+    {
+        public int Id_Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsRead { get; set; }
+        public string sender { get; set; }
+        public ICollection<MonAn> MonAn { get; set; }
+
+        public Email()
+        {
+            MonAn = new HashSet<MonAn>();
+        }
+    }
+
+    public class MonAn
+    {
+        public int Id_MonAn { get; set; }
+        public string TenMonAn { get; set; }
+        public string imageURI { get; set; }
+
+        public int Id_Email { get; set; }
+
+        // nav property
+        public virtual Email Email { get; set; }
+    }
+}
